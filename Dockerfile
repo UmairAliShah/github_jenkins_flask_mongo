629eb6ec3af6000f24cb53635efb202a484ef0b5
Suleman Cheema <suleman.cheema@northbaysolutions.net>
2020-01-27 18:46:15 +0500

    commiting flask dockerfile dockercompose

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.6-alpine
+
+RUN mkdir -p /flask_docker
+
+COPY /flask_docker /flask_docker
+
+RUN apk add --no-cache git gcc python3-dev gpgme-dev libc-dev
+
+RUN pip install -r flask_docker/requirements.txt
+
+EXPOSE 5011
+
+CMD ["python", "flask_docker/flask_docker.py"]
---
+++ flask_docker/flask_docker.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+from pymongo import MongoClient
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+import datetime
+# connect to MongoDB, change the \
+# << MONGODB URL >> to reflect your own connection string
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    client = MongoClient("54.87.133.253:27017")
+    databases = client.list_database_names()
+    for database in databases:
+        # databases
+        pprint("database::" + str(database))
+    return str(database)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port='5011',debug=True)
